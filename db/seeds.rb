66daffc6968d1acfff0fd66681df6ef0e81ab487
Blake A. Long <blake.a.long@gmail.com>
2019-09-12 11:29:02 -0600

    reseed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,14 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 # User.destroy_all
-# Sighting.destroy_all
+Sighting.destroy_all
 
-Sighting.create(animal: "deer", location: "alma, co", date: "9/3/2019", time: "9:00AM", note: "Male with only one antler", user_id: 7)
+Sighting.create(animal: "Woodpecker", location: "Fountain, CO", date: "9/3/2019", time: "9:00AM", note: "Little bugger woke me up at 4am!", user_id: 8, latitude: 38.693787, longitude: -104.698156)
 
-Sighting.create(animal: "fox", location: "breckenridge, co", date: "9/4/2019", time: "9:45AM", note: "Saw it waiting patiently to cross HWY 9!", user_id: 7)
+Sighting.create(animal: "Fox", location: "Breckenridge, co", date: "9/4/2019", time: "9:45AM", note: "Saw it waiting patiently to cross HWY 9!", user_id: 8, latitude: 38.693787, longitude: -104.698156)
 
-Sighting.create(animal: "rabbit", location: "denver, co", date: "8/28/2019", time: "10:34AM", note: "Hanging in a parking lot", user_id: 7)
+Sighting.create(animal: "Rabbit", location: "Denver, co", date: "8/28/2019", time: "10:34AM", note: "Hanging in a parking lot eating some trash.", user_id: 8, latitude: 39.7645187, longitude: -104.9951942,)
 
-Sighting.create(animal: "deer", location: "morrison, co", date: "9/1/2019", time: "7:00AM", note: "Off HWY 285", user_id: 7)
+Sighting.create(animal: "Deer", location: "Lochbuie, CO", date: "9/1/2019", time: "7:00AM", note: "I've been passing by a group of deer off 76 near the 7-11 for th past couple of days. Be careful while driving!", user_id: 8, latitude: 40.009864, longitude: -104.708080)
 
-User.create(first_name: "Blake", last_name: "Long", city: "Denver", state: "CO", email: "hardfoe@yahoo.com")
+# User.create(first_name: "Blake", last_name: "Long", city: "Denver", state: "CO", email: "hardfoe@yahoo.com")
